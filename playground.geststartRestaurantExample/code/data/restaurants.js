70ce987b76fadba325ecb3c1ebcfdc551336edc1
jsh <jsh440274>
2020-09-24 11:16:49 +0900

    first commit

---
+++ playground.geststartRestaurantExample/code/data/restaurants.js
@@ -0,0 +1,86 @@
+module.exports = [
+  {
+    name: "황소곱창",
+    location: "경기도 수원시 영통구 망포동 524-4",
+    url: "https://www.google.com/maps/place/%ED%99%A9%EC%86%8C%EA%B3%B1%EC%B0%BD/@37.2396731,127.0563909,17z/data=!3m1!4b1!4m5!3m4!1s0x357b448da6ed91f1:0xdaada6479465bccd!8m2!3d37.2396688!4d127.0585796",
+    flag: false,
+    point:{
+      point:{
+        latitude: 37.239934,
+        longitude: 127.058633
+      }
+    }
+  },
+  {
+    name: "일오닭갈비",
+    location: "경기도 고양시 일산동구 장항동 중앙로 1305-56",
+    url: "https://www.google.com/maps/place/%EC%9D%BC%EC%98%A4%EB%8B%AD%EA%B0%88%EB%B9%84+%EB%9D%BC%ED%8E%98%EC%8A%A4%ED%83%80+%EB%B3%B8%EC%A0%90/@37.6607737,126.7663542,17z/data=!3m1!4b1!4m5!3m4!1s0x357c855b39a5895f:0x481c895a3dae21d7!8m2!3d37.6607695!4d126.7685429",
+    flag: false,
+    point:{
+      point:{
+        latitude: 37.661041, 
+        longitude: 126.768500
+      }
+    }
+  },
+  {
+    name: "고요남",
+    location: "경기도 수원시 팔달구 인계동 1022-2",
+    url: "https://www.google.com/maps/place/%EA%B3%A0%EC%9A%94%EB%82%A8+%EC%88%98%EC%9B%90%EC%9D%B8%EA%B3%84%EB%8F%99/@37.2629504,127.0505472,14z/data=!4m8!1m2!3m1!2z6rOg7JqU64KoIOyImOybkOyduOqzhOuPmQ!3m4!1s0x0:0x76653fc14ebc7438!8m2!3d37.2668084!4d127.0289415",
+    flag: false,
+    point:{
+      point:{
+        latitude: 37.267048, 
+        longitude: 127.029025
+      }
+    }
+  },  
+  {
+    name: "What A Salad",
+    location: "서울특별시 용산구 이태원1동 보광로59길 31",
+    url: "https://www.google.com/maps/place/What+A+Salad/@37.5337836,126.9900109,17z/data=!3m1!4b1!4m5!3m4!1s0x357ca24a06f4144b:0x7850bcc41ed92cd9!8m2!3d37.5337794!4d126.9921996",
+    flag: false,
+    point:{
+      point:{
+        latitude: 37.533835,
+        longitude: 126.992170
+      }
+    }
+  },
+  {
+    name: "텍사스데브라질",
+    location: "서울특별시 서초구 사평대로 205",
+    url: "https://www.google.com/maps/place/%ED%85%8D%EC%82%AC%EC%8A%A4%EB%8D%B0%EB%B8%8C%EB%9D%BC%EC%A7%88+%EC%84%BC%ED%8A%B8%EB%9F%B4%EC%8B%9C%ED%8B%B0%EC%A0%90+TEXAS+de+BRAZIL+Restaurante+brasileiro/@37.5044295,127.0055714,17z/data=!3m1!4b1!4m5!3m4!1s0x357ca17b5323627b:0x322bb884a2ade538!8m2!3d37.5044253!4d127.0077601",
+    flag: false,
+    point:{
+      point:{
+        latitude: 37.504402,
+        longitude: 127.007757
+      }
+    }
+  },
+  {
+    name: "까치집",
+    location: "대전광역시 유성구 대학로145번길 47",
+    url: "https://www.google.com/maps/place/%EB%8C%80%EC%A0%84%EA%B4%91%EC%97%AD%EC%8B%9C+%EC%9C%A0%EC%84%B1%EA%B5%AC+%EC%98%A8%EC%B2%9C2%EB%8F%99+%EB%8C%80%ED%95%99%EB%A1%9C145%EB%B2%88%EA%B8%B8+47/@36.3630705,127.3490383,19z/data=!3m1!4b1!4m5!3m4!1s0x35654ba2a0c21587:0x26fd178bfbe46876!8m2!3d36.3630705!4d127.3495855",
+    flag: false,
+    point:{
+      point:{
+        latitude: 36.363129,   
+        longitude: 127.349580
+      }
+    }
+  },
+  {
+    name: "우리들의약국",
+    location: "대전광역시 유성구 궁동 397-8",
+    url: "https://www.google.com/maps/place/%EC%9A%B0%EB%A6%AC%EB%93%A4%EC%9D%98%EC%95%BD%EA%B5%AD/@36.362882,127.3485864,19z/data=!3m1!4b1!4m5!3m4!1s0x35654b98747b256f:0x3e6b10737fcfcdb3!8m2!3d36.362882!4d127.3491336",
+    flag: false,
+    point:{
+      point:{
+        latitude: 36.362906,  
+        longitude: 127.349134
+      }
+    }
+  }
+ ]
